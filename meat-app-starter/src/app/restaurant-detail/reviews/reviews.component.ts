5a9dfafa6065263513265126a0856dbeb3ae5637
Jefferson paulino <jpnr2013@gmail.com>
2019-07-13 17:52:27 -0300

    criado shopping-cart-service adicionando itens ao carrinho de compras

--- meat-app-starter/src/app/restaurant-detail/reviews/reviews.component.ts
+++ meat-app-starter/src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,27 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
 import {Observable} from 'rxjs'
 import {ActivatedRoute} from '@angular/router'
 import {RestaurantsService} from '../../restaurants/restaurants.service'
-import {MenuItem} from '../menu-item/menu-item.model'
 
 @Component({
-  selector: 'mt-menu',
-  templateUrl: './menu.component.html'
+  selector: 'mt-reviews',
+  templateUrl: './reviews.component.html'
 })
-export class MenuComponent implements OnInit {
+export class ReviewsComponent implements OnInit {
 
-  menu: Observable<MenuItem[]>
+  reviews: Observable<any>
 
   constructor(private restaurantsService: RestaurantsService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.menu = this.restaurantsService.menuOfRestaurant(this.route.parent.snapshot.params['id'])
-  }
-
-  addMenuItem(item: MenuItem){
-    console.log(item)
+    //como eu estou em um sublink preciso pegar o id do Pai, por isso deve-se colocar o parent
+    this.reviews = this.restaurantsService.reviewsOfRestaurant(this.route.parent.snapshot.params['id'])
   }
 
 }
--- meat-app-starter/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -5,6 +5,7 @@ import {MEAT_API} from "../app.api"
 import {Observable} from "rxjs/Observable";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/catch";
+import {MenuItem} from '../restaurant-detail/menu-item/menu-item.model'
 // import {ErrorHandler} from '../app.error-handler'
 
 @Injectable()
@@ -22,6 +23,16 @@ export class RestaurantsService {
 
   restaurantById(id: String): Observable<Restaurant>{
     return this.http.get(`${MEAT_API}/restaurants/${id}`).map(response => response.json())
+    // .catch(ErrorHandler.handleError)
+  }
+
+  reviewsOfRestaurant(id: string): Observable<any>{
+    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`).map(response => response.json())
+    // .catch(ErrorHandler.handleError)
+  }
+
+  menuOfRestaurant(id: string): Observable<MenuItem[]>{
+    return this.http.get(`${MEAT_API}/restaurants/${id}/menu`).map(response => response.json())
     // .catch(ErrorHandler.handleError)
   }
 
