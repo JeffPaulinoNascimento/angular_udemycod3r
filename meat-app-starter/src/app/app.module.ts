dd6f16ce993ab31c54a2aa6aded15c78b5c3e611
Jefferson paulino <jpnr2013@gmail.com>
2019-07-21 15:43:00 -0300

    criando o componente do total de compra

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -22,7 +22,8 @@ import { OrderComponent } from './order/order.component'
 import {FormsModule} from '@angular/forms';
 import { InputComponent } from './shared/input/input.component';
 import { RadioComponent } from './shared/radio/radio.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component';
+import { OrderItemsComponent } from './order/order-items/order-items.component'
+import {OrderService} from './order/order.service';
 import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component'
 
 @NgModule({
@@ -50,7 +51,7 @@ import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.co
     FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [RestaurantsService, ShoppingCartService, OrderService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- meat-app-starter/src/app/order/order.component.ts
+++ meat-app-starter/src/app/order/order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {RadioOption} from '../shared/radio/radio-option.model'
+import {OrderService} from './order.service'
+import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model'
 
 @Component({
   selector: 'mt-order',
@@ -15,9 +17,30 @@ export class OrderComponent implements OnInit {
     {label: 'Cartão Refeição', value:'REF' }
   ]
 
-  constructor() { }
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+  }
+
+  itemsValue(): number{
+    return this.orderService.itemsValue()
+
+  }
+
+  cartItems(): CartItem[]{
+    return this.orderService.cartItems()
+  }
+
+  increaseQty(item: CartItem){
+    this.orderService.increaseQty(item)
+  }
+
+  decreaseQty(item: CartItem){
+    this.orderService.decreaseQty(item)
+  }
+
+  remove(item: CartItem){
+    this.orderService.remove(item)
   }
 
 }
